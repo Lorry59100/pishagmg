e111ee8007fbf8ee192608b709648b91c173f759
pisha . <pisha@hotmail.fr>
2025-06-04 17:48:44 +0200

    [LC][FIX] - fixing basepath

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/PishaGaming",
+// Obtenez __dirname dans un module ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ mode }) => {
+  // Obtenez le chemin absolu du répertoire de travail actuel
+  const root = path.resolve(__dirname, '..');
+
+  // Charger les variables d'environnement en fonction du mode
+  const env = loadEnv(mode, root, '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/PishaGaming',
+  };
 });
